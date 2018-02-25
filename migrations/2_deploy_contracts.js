a26660cbe03fe340c19d2134a83aa1136d893af5
insideoutzombie <david@web3devs.com>
2018-02-25 17:47:53 -0600

    minor things to EthTipJar.sol

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var EthTipJar = artifacts.require("./EthTipJar.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
+  deployer.deploy(EthTipJar);
 };
+  
