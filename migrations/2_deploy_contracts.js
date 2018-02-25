5bdb038500082f2fa6a138cf5e05a09f6fd2293c
insideoutzombie <david@web3devs.com>
2018-02-25 17:51:46 -0600

    declares EthTipJar with let

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,8 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-<<<<<<< HEAD
-var EthTipJar = artifacts.require("./EthTipJar.sol");
-=======
 let EthTipJar = artifacts.require("./EthTipJar.sol");
->>>>>>> 79943e797d9c3e8f216bcba6926c394bfe9831d8
+
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
   deployer.deploy(EthTipJar);
 };
-  
